b2a4b58b36a99623c3bc8e616c4799d8262ef113
Halo <55074449+johnzhang27@users.noreply.github.com>
2020-10-20 21:41:08 -0600

    Update schedule

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import time
 import sys
 import os
 import datetime
+import schedule
 
 def auto_run():
-    while True:
-        os.system('python3 web_crawler.py')
-        time.sleep(86400)
+    os.system('python3 web_crawler.py')
 
-auto_run()
+schedule.every().day.at("19:30").do(auto_run)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
