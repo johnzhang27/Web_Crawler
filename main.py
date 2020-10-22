17fa093e277964c006d078ed9d5855c4d0639463
Halo <55074449+johnzhang27@users.noreply.github.com>
2020-10-21 20:50:37 -0600

    Update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import schedule
 def auto_run():
     os.system('python3 web_crawler.py')
 
-schedule.every().day.at("19:30").do(auto_run)
+schedule.every().day.at("20:50").do(auto_run)
 
 while 1:
     schedule.run_pending()
